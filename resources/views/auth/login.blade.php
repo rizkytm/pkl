05b1ec0b7faac67a7d5337674d5bb8cb23f1ef60
AzmanFatahillah <azman.fatahillah1@gmail.com>
2018-07-31 13:57:32 +0700

    logo siw

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>AdminLTE 2 | Log in</title>
+  <link rel="icon" type="image/png" href="{{ asset('asset/dist/img/logo.png') }}">
+  <title>Masuk | SIW</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -31,7 +32,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Sistem INformasi</b> Wawancara</a>
+    <a href="../../index2.html"><b>Sistem Informasi</b> Wawancara</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
