d666f6cc73ff79bcb373df272f61e3d817c819b8
fajaranggara <fajartrianggara1@gmail.com>
2018-08-23 14:37:32 +0700

    keterangan

--- app/Http/Controllers/WORDController.php
+++ app/Http/Controllers/WORDController.php
@@ -18,67 +18,72 @@ class WORDController extends Controller
 {
     public function createWordDocx($id)
     {
-    	$wordTest = new \PhpOffice\PhpWord\PhpWord();
-    	$newSection = $wordTest->addSection(array('paperSize' => 'A4', 'marginLeft' => 1500, 'marginRight' => 1500, 'marginTop' => 1500, 'marginBottom' => 1500));
-
-
     	$posts = Post::find($id);
         $cid = $posts->category_id;
         $pid = $posts->id;
         $categories = Category::where("id", $cid)->first();
-        $posting = Post::with('narasumber')->get();
+        //$posting = Post::with('narasumber')->where('post_id', $pid)->get();
     	$answers = Answer::with('question')->where('post_id', $pid)->get();
-        $narasumber = $posts->narasumber()->get();
+        //$narasumber = $posts->narasumber()->get();
     	
+        $wordTest = new \PhpOffice\PhpWord\PhpWord();
+        $newSection = $wordTest->addSection(array('paperSize' => 'A4', 'marginLeft' => 1500, 'marginRight' => 1500, 'marginTop' => 1500, 'marginBottom' => 1500));
 
 
-  //   	$styleTable = array('borderSize' => 6, 'borderColor' => '000000', 'cellMargin' => 80);
-		// $styleFirstRow = array('borderBottomSize' => 18, 'borderBottomColor' => '0000FF', 'bgColor' => 'FFFFFF');
-		// $styleCell = array('valign' => 'center');
-		// //$styleCellBTLR = array('valign' => 'center', 'textDirection' => \PhpOffice\PhpWord\Style\Cell::TEXT_DIR_BTLR);
-		// $fontStyle = array('bold' => true, 'align' => 'center');
-		// $wordTest->addTableStyle('Fancy Table', $styleTable, $styleFirstRow);
-		// $table = $newSection->addTable('Fancy Table');
-		// $table->addRow(100);
-		// $table->addCell(5000, $styleCell)->addText('Penulis');
-		// $table->addCell(5000, $styleCell)->addText('1. '.$posts->penulis1 . '
-		// 	' . '2. '.$posts->penulis2, $fontStyle);
-		//$table->addCell(2000, $styleCell)->addText($posts->topic, $fontStyle);
-		
-
-    	$fontStyle = array('name' => 'Times New Roman', 'size' => 14, 'space' => array('before' => 360, 'after' => 480));
-    	$newSection->addText('Penulis           : ' . $posts->penulis1 . ' dan ' . $posts->penulis2, $fontStyle);
-    	$newSection->addText('Topik             : ' . $posts->topic, $fontStyle);
-        $newSection->addText('Lembaga        : ' . $posts->lembaga, $fontStyle);
-        
-        $newSection->addText('Narasumber   : ', $fontStyle);
-        $i = 1;
-        foreach ($posting as $posti) {
-            foreach ($posti->narasumber()->get() as $nara){
-                $newSection->addText('                      ' .$i.'. ' .$nara->nama. ' ('.$nara->kontak. ')', $fontStyle);
-                $i++;
+        $fontStyle = array('name' => 'Times New Roman', 'size' => 14, 'space' => array('before' => 360, 'after' => 480));
+            $newSection->addText('Penulis           : ' . $posts->penulis1 . '; ' . $posts->penulis2, $fontStyle);
+            $newSection->addText('Topik             : ' . $posts->topic, $fontStyle);
+            $newSection->addText('Lembaga        : ' . $posts->lembaga, $fontStyle);
+            
+            $newSection->addText('Narasumber   : ', $fontStyle);
+            
+            $i = 1;
+            foreach ($posts->narasumber as $nara){
+                if(($nara->nama)!=NULL){
+                    $newSection->addText('                      ' .$i.'. ' .$nara->nama. ' ('.$nara->kontak. ')', $fontStyle);
+                    $i++;
+                }
             }
+            
+            
+
+		
+        if(($posts->isi)==NULL)
+        {
+        	
+            $lineStyle = array('weight' => 2, 'width' => 443, 'height' => 0, 'color' => 000000);
+            $newSection->addLine($lineStyle);
+
+            $fontQ = array('name' => 'Times New Roman', 'bold' => TRUE, 'size' => 12);
+            $fontA = array('name' => 'Times New Roman', 'size' => 12);
+            $para = array('space' => array('before' => 250, 'after' => 50));
+            $i = 1;
+        	foreach ($answers as $answer) {
+        		foreach ($answer->question()->get() as $question) {
+        			$newSection->addText($i . '. ' .
+        				$question->question, 
+        				$fontQ,
+                        $para
+        			);
+                    $i++;
+        			$newSection->addText('   ' .$answer->answer, $fontA);
+        		}   		
+        	}
+        } else{
+            $lineStyle = array('weight' => 2, 'width' => 443, 'height' => 0, 'color' => 000000);
+            $newSection->addLine($lineStyle);
+
+            $fontQ = array('name' => 'Times New Roman', 'bold' => TRUE, 'size' => 12);
+            $fontA = array('name' => 'Times New Roman', 'size' => 12);
+            $para = array('space' => array('before' => 250, 'after' => 50));
+            
+            
+                    $newSection->addText($posts->isi, 
+                        $fontA,
+                        $para
+                    );        
+            
         }
-        
-
-        $lineStyle = array('weight' => 2, 'width' => 443, 'height' => 0, 'color' => 000000);
-        $newSection->addLine($lineStyle);
-
-        $fontQ = array('name' => 'Times New Roman', 'bold' => TRUE, 'size' => 12);
-        $fontA = array('name' => 'Times New Roman', 'size' => 12);
-        $para = array('space' => array('before' => 250, 'after' => 50));
-        $i = 1;
-    	foreach ($answers as $answer) {
-    		foreach ($answer->question()->get() as $question) {
-    			$newSection->addText($i . '. ' .
-    				$question->question, 
-    				$fontQ,
-                    $para
-    			);
-                $i++;
-    			$newSection->addText('   ' .$answer->answer, $fontA);
-    		}   		
-    	}
     		
 
     	$objectWriter = \PhpOffice\PhpWord\IOFactory::createWriter($wordTest, 'Word2007');
